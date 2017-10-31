3913a5dceaa86b22adeadf2070de01fb06f1a7ac
alexirion10 <alexirion10@gmail.com>
2017-10-30 21:08:19 -0500

    calculating averages and begin/end range deltas

--- predictor.py
+++ predictor.py
@@ -27,6 +27,9 @@ class CompanyListing:
             total += float(self.quotes[index])
         return str(total / 120)
 
+    def getBeginEndDiff(self):
+        return self.quotes[len(self.quotes)-1] - self.quotes[0]
+
 inputFile = xlrd.open_workbook("workbook1.xlsx")
 print("The number of sheets is {0}".format(inputFile.nsheets))
 # print("Worksheet name(s): {0}".format(inputFile.sheet_names()))
@@ -37,11 +40,11 @@ for ry in range(dataGrid.ncols):
     curCompany = CompanyListing(dataGrid.cell_value(0, ry))
     companies.append(curCompany)
     for rx in range(dataGrid.nrows):
-        if(ry > 0):
+        if(rx != 0):
             curCompany.addQuote(dataGrid.cell_value(rx, ry))
 
-# for companyIndex in range(len(companies)):
-#     curCompany = companies[companyIndex]
-#     # print(curCompany.name + " " + curCompany.avePrice())
-#     print(curCompany.getNumPrices())
+for companyIndex in range(len(companies)):
+    curCompany = companies[companyIndex]
+    print(curCompany.name + " " + curCompany.avePrice())
+    print("--->" + str(curCompany.getBeginEndDiff()))
 

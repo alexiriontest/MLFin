7f888699c7fa078aa729e932acaed066dc383567
Alex Irion <alexirion@alexs-air.attlocal.net>
2017-10-30 20:53:47 -0500

    reading in more data now

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 MLFin project
 
 updated by Alex Irion to calculate future stock market trends
+
+ToDo:
+	- add ML library
+	- add graphing
+	- write C# or C++ client to read Excel document
+	- get more statistical data /quotes
--- predictor.py
+++ predictor.py
@@ -1,14 +1,47 @@
 # machine learning stock market predictions
 import sys
 import xlrd
+# import plotly
+# import numpy
+# 
+companies = []
+
+class CompanyListing:
+    
+    def __init__(self, name):
+      self.name = name
+      self.quotes = []
+
+    def getQuote(self, index):
+        return self.quotes[index]
+
+    def getNumPrices(self):
+        return len(self.quotes)
+
+    def addQuote(self, value):
+        self.quotes.append(value)
+
+    def avePrice(self):
+        total = 0
+        for index in range(len(self.quotes)):
+            total += float(self.quotes[index])
+        return str(total / 120)
 
 inputFile = xlrd.open_workbook("workbook1.xlsx")
 print("The number of sheets is {0}".format(inputFile.nsheets))
 # print("Worksheet name(s): {0}".format(inputFile.sheet_names()))
-dataGrid = file.sheet_by_index(1)
-print("Sheet ~{0}~ is: {1}x{2}".format(sheetTwo.name, sheetTwo.nrows, sheetTwo.ncols))
+dataGrid = inputFile.sheet_by_index(1)
+print("Sheet ~{0}~ is: {1}x{2}".format(dataGrid.name, dataGrid.nrows, dataGrid.ncols))
 
-for ry in range(sheetTwo.nrows):
-	for rx in range(sheetTwo.ncols):
-		if(rx == 0):
-			print(sheetTwo.cell_value(rx, ry))
+for ry in range(dataGrid.ncols):
+    curCompany = CompanyListing(dataGrid.cell_value(0, ry))
+    companies.append(curCompany)
+    for rx in range(dataGrid.nrows):
+        if(ry > 0):
+            curCompany.addQuote(dataGrid.cell_value(rx, ry))
+
+# for companyIndex in range(len(companies)):
+#     curCompany = companies[companyIndex]
+#     # print(curCompany.name + " " + curCompany.avePrice())
+#     print(curCompany.getNumPrices())
+
